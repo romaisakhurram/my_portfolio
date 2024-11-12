e866a96c0fc8f9d3c18bcc71708678ff15695890
*romaisa <*mahamkhurram1230@gmail.com>
2024-11-12 17:20:01 +0500

    update

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,7 +1,7 @@
 
 export default function About () {
     return (
-        <div className="container pt-32">
+        <div id="About"className="container pt-32">
             <h2 className="text-[50px]" data-aos="zoom-in-down">About Me</h2>
             <p className="text-gray-500" data-aos="zoom-in-down">  I am a Student of GIAIC and also create functional, user-friendly websites and web applications. They may write code, develop and test new applications, or monitor site performance and traffic. Front-end developers focus on the user-facing side of their work, while back-end developers make websites functional and secure.</p>
 
